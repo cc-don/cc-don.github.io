06a6666f95364986c62ec45568379f63fb32a3a8
cc-don <132142157+cc-don@users.noreply.github.com>
2023-04-29 23:30:01 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head lang="ja">
+<meta charset="utf-8">
+<title>ccdon test bbs</title>
+</head>
+<body>
+<h1>ccdon test bbs</h1>
+
+
+<form action="" method="post">
+    内容:<input type="text" name="message">
+    ユーザー名:<input type="text" name="user_name">
+    <input type="submit" name="send_message" value="Post">
+</form>
+
+<h2>投稿一覧</h2>
+<ul>
+
+</ul>
+</body>
+</html>
